90fefd3da8ecddfe0794ff81154cfe047f8780a6
BrianDGLS <briandgls92@gmail.com>
2023-02-07 09:12:48 +0000

    feat: update example code

--- src/pong.ts
+++ src/pong.ts
@@ -1,104 +1,233 @@
-import kaboom, { DrawTextOpt, Vec2 } from "kaboom";
-
-const $canvas = document.querySelector("#pong");
+import kaboom from "kaboom";
 
 kaboom({
   width: 640,
   height: 480,
   font: "sinko",
   background: [0, 0, 0],
-  canvas: $canvas as HTMLCanvasElement,
 });
 
-loadSound("hit", "./hit.wav");
+loadSound("hit", "hit.wav");
 
-scene("main", () => {
-  let playerServe = true;
+function clamp(value: number, min: number, max: number): number {
+  if (value < min) {
+    return min;
+  }
 
-  add([pos(width() / 2 - 4, 0), rect(2, height()), outline(1)]);
+  if (value > max) {
+    return max;
+  }
 
-  add(["top", area(), rect(width(), 0), color(0, 0, 0), pos(0, 0)]);
-  add(["bottom", area(), rect(width(), 0), color(0, 0, 0), pos(0, height())]);
-  add(["right", area(), rect(0, height()), color(0, 0, 0), pos(width(), 0)]);
-  add(["left", area(), rect(0, height()), color(0, 0, 0), pos(0, 0)]);
+  return value;
+}
 
-  const player = add([
-    "player",
-    "paddle",
-    area(),
-    rect(6, 60),
-    color(255, 255, 255),
-    pos(width() - 40, height() / 2),
-    {
-      speed: 160,
-    },
-  ]);
-
-  const opponent = add([
-    "opponent",
-    "paddle",
-    area(),
-    rect(6, 60),
-    color(255, 255, 255),
-    pos(40, height() / 2),
-    {
-      speed: 160,
-      moveToBall(ball) {
-        if (ball.velocity.x < 0) {
-          opponent.moveTo(
-            opponent.pos.x,
-            ball.pos.y - ball.radius,
-            opponent.speed
-          );
-        } else {
-          opponent.moveTo(
-            opponent.pos.x,
-            height() / 2 - opponent.height / 2,
-            opponent.speed
-          );
-        }
-      },
-    },
-  ]);
-
-  const score = add([
-    text("0   0", {
-      size: 48,
-    }),
+scene("game-over", (playerScore: number, opponentScore: number) => {
+  add([
+    text(`${playerScore} ${opponentScore}`, { size: 48 }),
+    pos(width() / 2, 40),
     (origin as any)("center"),
-    color(150, 150, 150),
+  ]);
+
+  const gameOverText = playerScore > opponentScore ? "You win!" : "You lose!";
+  add([
+    text(gameOverText, { size: 32 }),
     pos(width() / 2, 80),
+    (origin as any)("center"),
+  ]);
+
+  const btn = add([
+    text("Click to play again", { size: 32 }),
+    pos(width() / 2, height() / 2),
+    area({ cursor: "pointer" }),
+    (origin as any)("center"),
+  ]);
+
+  btn.onClick(() => {
+    go("game");
+  });
+});
+
+scene("menu", () => {
+  add([
+    text(`Pong`, { size: 64 }),
+    pos(width() / 2, 60),
+    (origin as any)("center"),
+  ]);
+
+  const btn = add([
+    text("Click to play", { size: 32 }),
+    pos(width() / 2, height() / 2),
+    area({ cursor: "pointer" }),
+    (origin as any)("center"),
+  ]);
+
+  btn.onClick(() => {
+    go("game");
+  });
+});
+
+scene("game", () => {
+  /**
+   * Score
+   */
+  const score = add([
+    text("0 0", { size: 48 }),
+    pos(width() / 2, 40),
+    (origin as any)("center"),
     {
-      player: 0,
-      opponent: 0,
+      playerScore: 0,
+      opponentScore: 0,
       winningScore: 3,
-      getText() {
-        return `${this.opponent}   ${this.player}`;
+      playerWins(): boolean {
+        return this.playerScore >= this.winningScore;
+      },
+      opponentWins(): boolean {
+        return this.opponentScore >= this.winningScore;
       },
     },
   ]);
 
+  score.onUpdate(() => {
+    score.text = `${score.playerScore} ${score.opponentScore}`;
+
+    if (score.playerWins() || score.opponentWins()) {
+      go("game-over", score.playerScore, score.opponentScore);
+    }
+  });
+
+  /**
+   * Ball
+   */
   const ball = add([
     "ball",
     circle(6),
     area({ width: 12, height: 12, offset: vec2(-6) }),
-    color(255, 255, 255),
     pos(width() / 2, height() / 2),
+    color(255, 255, 255),
     {
       speed: 200,
-      velocity: vec2(0, 0),
-      serve() {
-        ball.velocity.x = playerServe ? -ball.speed : ball.speed;
-        ball.velocity.y = rand() * (chance(0.5) ? ball.speed : -ball.speed);
-        playerServe = playerServe;
+      serveLeft: true,
+      initialSpeed: 200,
+      speedIncrement: 15,
+      velocity: vec2(),
+      serve(xVelocity: number) {
+        this.velocity.x = xVelocity;
+        this.setYVelocity(chance(0.5));
+        this.serveLeft = !this.serveLeft;
+      },
+      setYVelocity(up: boolean) {
+        const yVelocity = rand(this.speed / 4, this.speed);
+        this.velocity.y = up ? -yVelocity : yVelocity;
       },
       reset() {
-        this.pos.x = width() / 2;
-        this.pos.y = height() / 2;
-        this.speed = 200;
+        this.moveTo(width() / 2, height() / 2);
+
+        this.velocity.x = 0;
+        this.velocity.y = 0;
+
+        this.speed = this.initialSpeed;
+
+        this.serve(this.serveLeft ? -this.speed : this.speed);
+      },
+      outToLeft(): boolean {
+        return this.pos.x + this.radius < 0;
+      },
+      outToRight(): boolean {
+        return this.pos.x - this.radius > width();
+      },
+      hasHitRoof(): boolean {
+        return this.pos.y - this.radius <= 0;
+      },
+      hasHitFloor(): boolean {
+        return this.pos.y + this.radius >= height();
       },
     },
   ]);
+
+  ball.onCollide("paddle", () => {
+    if (ball.velocity.x > 0) {
+      ball.velocity.x = -ball.speed;
+    } else {
+      ball.velocity.x = ball.speed;
+    }
+
+    ball.speed += ball.speedIncrement;
+  });
+
+  ball.onCollide("*", () => {
+    play("hit");
+  });
+
+  ball.onUpdate(() => {
+    ball.move(ball.velocity.x, ball.velocity.y);
+
+    const outToLeft = ball.outToLeft();
+    const outToRight = ball.outToRight();
+
+    if (outToLeft) {
+      score.playerScore += 1;
+    }
+
+    if (outToRight) {
+      score.opponentScore += 1;
+    }
+
+    if (outToLeft || outToRight) {
+      ball.reset();
+    }
+
+    if (ball.hasHitFloor()) {
+      ball.setYVelocity(true);
+      play("hit");
+    }
+
+    if (ball.hasHitRoof()) {
+      ball.setYVelocity(false);
+      play("hit");
+    }
+  });
+
+  /**
+   * Paddle
+   */
+  const paddle = [
+    "paddle",
+    rect(12, 80),
+    area(),
+    (origin as any)("center"),
+    {
+      speed: 200,
+      update() {
+        const halfHeight = this.height / 2;
+        this.pos.y = clamp(this.pos.y, halfHeight, height() - halfHeight);
+      },
+    },
+  ];
+
+  /**
+   * Player
+   */
+  const player = add(["player", ...paddle, pos(width() - 40, height() / 2)]);
+
+  /**
+   * Opponent
+   */
+  const opponent = add(["opponent", ...paddle, pos(40, height() / 2)]);
+
+  opponent.onUpdate(() => {
+    if (ball.velocity.x > 0) {
+      opponent.moveTo(opponent.pos.x, height() / 2, opponent.speed);
+    } else {
+      opponent.moveTo(opponent.pos.x, ball.pos.y, opponent.speed);
+    }
+  });
+
+  /**
+   * Global
+   */
+  onLoad(() => {
+    ball.serve(-ball.speed);
+  });
 
   onKeyDown("up", () => {
     player.move(0, -player.speed);
@@ -106,124 +235,6 @@ scene("main", () => {
 
   onKeyDown("down", () => {
     player.move(0, player.speed);
-  });
-
-  onLoad(() => {
-    ball.serve();
-  });
-
-  onUpdate(() => {
-    opponent.moveToBall(ball);
-    ball.move(ball.velocity);
-
-    if (
-      score.player >= score.winningScore ||
-      score.opponent >= score.winningScore
-    ) {
-      go("game-over", score.player, score.opponent);
-    }
-  });
-
-  let soundPlayed = false;
-  onCollide("ball", "*", () => {
-    if (!soundPlayed) {
-      play("hit");
-      soundPlayed = true;
-      setTimeout(() => {
-        soundPlayed = false;
-      }, 50);
-    }
-  });
-
-  onCollide("ball", "player", () => {
-    ball.velocity.x = -ball.speed;
-    ball.velocity.y = rand() * (chance(0.5) ? ball.speed : -ball.speed);
-  });
-
-  onCollide("ball", "paddle", () => {
-    ball.speed += 20;
-  });
-
-  onCollide("ball", "opponent", () => {
-    ball.velocity.x = ball.speed;
-    ball.velocity.y = rand() * (chance(0.5) ? ball.speed : -ball.speed);
-  });
-
-  onCollide("ball", "bottom", () => {
-    ball.velocity.y = rand() * -ball.speed;
-  });
-
-  onCollide("ball", "top", () => {
-    ball.velocity.y = rand() * ball.speed;
-  });
-
-  onCollide("ball", "left", () => {
-    score.player += 1;
-    score.text = score.getText();
-
-    ball.reset();
-    ball.serve();
-  });
-
-  onCollide("ball", "right", () => {
-    score.opponent += 1;
-    score.text = score.getText();
-
-    ball.reset();
-    ball.serve();
-  });
-
-  onCollide("paddle", "bottom", (paddle) => {
-    paddle.pos.y = height() - paddle.height;
-  });
-
-  onCollide("paddle", "top", (paddle) => {
-    paddle.pos.y = 0;
-  });
-});
-
-scene("menu", () => {
-  const btn = add([
-    text("Play", { size: 48 }),
-    pos(width() / 2, height() / 2),
-    area({ cursor: "pointer" }),
-    scale(1),
-    (origin as any)("center"),
-  ]);
-
-  btn.onClick(() => {
-    go("main");
-  });
-});
-
-scene("game-over", (playerScore: number, opponentScore: number) => {
-  add([
-    text(`${opponentScore} : ${playerScore}`, {
-      size: 24,
-    }),
-    (origin as any)("center"),
-    color(150, 150, 150),
-    pos(width() / 2, 80),
-  ]);
-
-  add([
-    text(playerScore > opponentScore ? "You Won!" : "You Lost!", {
-      size: 24,
-    }),
-    (origin as any)("center"),
-    color(150, 150, 150),
-    pos(width() / 2, 120),
-  ]);
-
-  const btn = add([
-    text("Play Again", { size: 48 }),
-    pos(width() / 2, height() / 2),
-    area({ cursor: "pointer" }),
-    (origin as any)("center"),
-  ]);
-
-  btn.onClick(() => {
-    go("main");
   });
 });
 
